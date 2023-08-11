83ff46b7459a3dd571868329422749d441241e60
김아진 <ajkim@wise.co.kr>
2023-08-11 14:19:24 +0900

    sty: es6에 맞게 소스 코드 수정

--- src/main/view/src/config/localization/index.js
+++ src/main/view/src/config/localization/index.js
@@ -1,11 +1,11 @@
-import LocalizedStrings from "react-localization";
-import ko from "./language/ko.json";
-import en from "./language/en.json";
+import LocalizedStrings from 'react-localization';
+import ko from './language/ko.json';
+import en from './language/en.json';
 
 const strings = new LocalizedStrings({
-    en,
-    ko
-})
+  en,
+  ko
+});
 
 // 만약 제대로 적용되지 않을시 Language 강제 설정
 // strings.setLanguage('ko');
--- src/main/view/src/config/theme/index.js
+++ src/main/view/src/config/theme/index.js
@@ -1,12 +1,12 @@
-import defaultColor from "./color/default.json"
-import defaultFont from "./color/default.json"
-import defaultMargin from "./color/default.json"
-import defaultSize from "./color/default.json"
+import defaultColor from './color/default.json';
+import defaultFont from './color/default.json';
+import defaultMargin from './color/default.json';
+import defaultSize from './color/default.json';
 
-import testColor from "./color/test.json"
-import testFont from "./color/test.json"
-import testMargin from "./color/test.json"
-import testSize from "./color/test.json"
+import testColor from './color/test.json';
+import testFont from './color/test.json';
+import testMargin from './color/test.json';
+import testSize from './color/test.json';
 
 let theme = {};
 
@@ -14,41 +14,38 @@ let theme = {};
  * 호출 시 테마 변경. theme는 참조를 반환하므로 해당 메서드 호출 시
  * 모든 컴포넌트에 적용된 테마값이 바뀜.
  * @param {String} themeName 입력시 입력된 테마 적용
- * @returns 없음.
  */
-const changeTheme = (themeName = "default") => {
-    const makeTheme = function(color, font, margin, size) {
-        theme.color = color;
-        theme.font = font;
-        theme.margin = margin;
-        theme.size = size;
-    }
+const changeTheme = (themeName = 'default') => {
+  const makeTheme = function(color, font, margin, size) {
+    theme.color = color;
+    theme.font = font;
+    theme.margin = margin;
+    theme.size = size;
+  };
 
-    if (themeName == "test") {
-        makeTheme(testColor, testFont, testMargin, testSize);
-    } else {
-        makeTheme(defaultColor, defaultFont, defaultMargin, defaultSize);
-    }
+  if (themeName == 'test') {
+    makeTheme(testColor, testFont, testMargin, testSize);
+  } else {
+    makeTheme(defaultColor, defaultFont, defaultMargin, defaultSize);
+  }
 };
 
 /**
- * 공통으로 공유하는 theme 객체가 return되므로 사용할 때 되도록 해당 객체 통째로 사용.
- * 
+ * 공통으로 공유하는 theme객체의 참조가 return되므로 사용할 때 되도록 해당 객체 통째로 사용.
  * ex)
  * ${theme.color.background} (O)
- *     
  * const bg = theme.color.background (X)
  * @param {String} themeName 테마 이름 입력시 테마 바뀜.
- * @returns 
+ * @return {JSON} 테마 참조 파일
  */
 const getTheme = (themeName) => {
-    if (themeName) {
-        changeTheme(themeName);
-    } else if(!theme.color) {
-        changeTheme();
-    }
+  if (themeName) {
+    changeTheme(themeName);
+  } else if (!theme.color) {
+    changeTheme();
+  }
 
-    return theme;
-}
+  return theme;
+};
 
 export { getTheme, changeTheme };
