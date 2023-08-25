732df8ddd64d598402e07437769684539c8223bb
mjhwang <mjhwang@wise.co.kr>
2023-08-25 10:14:07 +0900

    Revert "Delete RibbonImageButton.jsx"
    This reverts commit 22e8c11d4d7ed38da28415c2a4bb0caf0acf8f57.

---
+++ src/main/view/src/components/common/atoms/RibbonImageButton.jsx
@@ -0,0 +1,72 @@
+import {styled, css} from 'styled-components';
+import React, {useState} from 'react';
+import {getTheme} from '../../../config/theme';
+import {hexToRgb, rgbToFilterString} from 'components/utils/Color';
+
+const theme = getTheme();
+
+const Button = styled.div`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  cursor: pointer;
+
+  &:active{
+    transform: scale(0.9, 0.9);
+    transition: transform .2s;
+  }
+  
+  ${(props) => props.hoveranimation && css`
+    &:hover, &:active{
+      ${rgbToFilterString(hexToRgb(theme.color.primary))}
+    }
+  `}
+`;
+
+const Image = styled.img`
+  height: 24px;
+  width: auto;
+`;
+
+/**
+ * hoveredImgSrc 없는 경우 primary 색상으로 hover 애니메이션 삽입
+ * width, height는 미입력시 기본값으로 적용
+ * onClick: 클릭 이벤트
+ * imgSrc: 버튼 이미지
+ * hoveredImgSrc: 마우스 오버시 변할 이미지
+ * width: 컴포넌트 넓이(기본값 60px)
+ * height: 컴포넌트 높이(기본값 60px)
+ * @param {any} props
+ * @return {Component}
+ */
+const RibbonImageButton = ({
+  onClick, imgSrc, hoveredImgSrc, width='60px', height='60px', ...props
+}) => {
+  const [src, setSrc] = useState(imgSrc);
+
+  const onMouseOver = function() {
+    setSrc(hoveredImgSrc);
+  };
+
+  const onMouseOut = function() {
+    setSrc(imgSrc);
+  };
+
+  return (
+    <Button
+      width={width}
+      height={height}
+      hoveranimation={hoveredImgSrc? '' : 'true'}
+      onClick={onClick}
+      onMouseOver={hoveredImgSrc? onMouseOver : null}
+      onMouseOut={hoveredImgSrc? onMouseOut : null}
+      {...props}
+    >
+      <Image
+        src={src}
+        width={width}
+      />
+    </Button>
+  );
+};
+
+export default React.memo(RibbonImageButton);
