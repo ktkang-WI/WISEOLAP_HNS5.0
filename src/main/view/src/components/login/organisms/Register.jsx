ca3d5dc2a77f5948148a66330da52c8f96b3073d
YoungGiLee <yglee93@wise.co.kr>
2023-09-05 15:10:18 +0900

    pref/login&register
    라우터에 로그인, 회원가입 페이지 작성.

---
+++ src/main/view/src/components/login/organisms/Register.jsx
@@ -0,0 +1,30 @@
+import {styled} from 'styled-components';
+// import '../SignIn.css';
+import TitleImage from '../atoms/TitleImage';
+import FormInputs from '../molecules/FormInputs';
+import inputElements from './InputElements';
+
+const StyledSignIn = styled.div`
+  margin-left: 50%;
+  min-height: inherit;
+`;
+
+const ActionPageWrap = styled.div`
+  width: 60%;
+  height: 100%;
+  padding: 134px 112px 0;
+`;
+
+const Register = () => {
+  return (
+    <StyledSignIn>
+      <ActionPageWrap>
+        <TitleImage type={'register'}/>
+        <FormInputs
+          contents={inputElements()['register']}
+        />
+      </ActionPageWrap>
+    </StyledSignIn>
+  );
+};
+export default Register;
