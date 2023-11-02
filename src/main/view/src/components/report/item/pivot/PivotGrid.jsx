b41883a7b317e818ff629c4ff5ddfd988e81ad44
wise-ajkim <99698967+wise-ajkim@users.noreply.github.com>
2023-11-02 15:32:55 +0900

    hotfix: 피벗그리드 리사이징 오류 수정

--- src/main/view/src/components/report/item/pivot/PivotGrid.jsx
+++ src/main/view/src/components/report/item/pivot/PivotGrid.jsx
@@ -1,20 +1,57 @@
-import DevPivotGrid from 'devextreme-react/pivot-grid';
-import React from 'react';
+import DevPivotGrid, {
+  FieldChooser,
+  Scrolling
+} from 'devextreme-react/pivot-grid';
+import {useEffect, useRef} from 'react';
 
 const PivotGrid = ({id, mart}) => {
   if (!mart.init) {
     return <></>;
   }
 
+  const ref = useRef();
+
+  useEffect(() => {
+    const item = ref.current;
+
+    if (!item) return;
+
+    const element = item._element;
+
+    // NOTE: Flex-layout 아이템 렌더링 => layout 리사이즈 사이클 이슈 때문에
+    // ResizeObserver 추가
+    const observer = new ResizeObserver(() => {
+      const instance = item.instance;
+
+      if (instance) {
+        instance.updateDimensions();
+      }
+    });
+
+    observer.observe(element);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  const onContentReady = (e, e2) => {
+    console.log(e);
+    console.log(e2);
+  };
+
   return (
     <DevPivotGrid
-      width='100%'
-      height='100%'
+      ref={ref}
       id={id}
       dataSource={mart.dataSourceConfig}
+      onOptionChanged={onContentReady}
+      wordWrapEnabled={false}
     >
+      <FieldChooser enabled={false}> </FieldChooser>
+      <Scrolling mode="virtual" />
     </DevPivotGrid>
   );
 };
 
-export default React.memo(PivotGrid);
+export default PivotGrid;
