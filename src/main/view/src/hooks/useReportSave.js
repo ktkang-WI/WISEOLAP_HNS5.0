0b81f90988153dfe9451119296600fd83759aa27
jins-sy <54256571+jin-sy@users.noreply.github.com>
2023-12-07 18:51:42 +0900

    pref: ReportMstrDTO.java fldName 필드 추가

--- src/main/view/src/hooks/useReportSave.js
+++ src/main/view/src/hooks/useReportSave.js
@@ -31,6 +31,7 @@ const useReportSave = () => {
     param.reportNm = dataSource.reportNm;
     param.fldId = dataSource.fldId;
     param.fldType = dataSource.fldType;
+    param.fldName = dataSource.fldName;
     param.reportOrdinal = dataSource.reportOrdinal;
     // TODO: reportType 비정형 개발 시 고려 우선 'DashAny' 로 하드 코딩
     // param.reportType = 'DashAny';
