b65f3a121d2592b9880ef1c94021842649c46da0
jins-sy <54256571+jin-sy@users.noreply.github.com>
2023-12-11 14:29:02 +0900

    pref: parameterSelector 중복 함수 제거

--- src/main/view/src/redux/selector/ParameterSelector.js
+++ src/main/view/src/redux/selector/ParameterSelector.js
@@ -6,9 +6,3 @@ export const selectCurrentParameters = createSelector(
     (state) => state.meta.parameter,
     (reportId, parameter) => parameter[reportId]
 );
-
-export const selectParameter = createSelector(
-    selectCurrentReportId,
-    (state) => state.meta.parameter,
-    (reportId, parameter) => parameter[reportId]
-);
