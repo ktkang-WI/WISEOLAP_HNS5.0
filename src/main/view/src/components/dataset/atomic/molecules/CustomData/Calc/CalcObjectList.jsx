0755673e1c37147dc2c4523f79580bdf491c5945
wise-ajkim <99698967+wise-ajkim@users.noreply.github.com>
2023-12-18 10:05:26 +0900

    perf: 사용자 정의 데이터 List Selection 로직 수정

--- src/main/view/src/components/dataset/atomic/molecules/CustomData/Calc/CalcObjectList.jsx
+++ src/main/view/src/components/dataset/atomic/molecules/CustomData/Calc/CalcObjectList.jsx
@@ -2,46 +2,31 @@ import Wrapper from 'components/common/atomic/Common/Wrap/Wrapper';
 import {dataSource}
   from 'components/dataset/atomic/organism/CustomData/Data/customObjectList';
 import {List, TextArea} from 'devextreme-react';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 
 // 사용자 정의 데이터 객체 정보 및 설명
 const CalcObjectList = () => {
-  // 열,상수,연산자,함수 값 저장
-  const [objectList, setObjectList] = useState();
   // 열,상수,연산자,함수 클릭값
   const [selectedObjectList, setSelectedObjectList] = useState();
-  // 열,상수,연산자,함수 내부 값
-  const [objectListContents, setObjectListContents] = useState([]);
   // 열,상수,연산자,함수 내부 값 사용설명
   const [explanation, setExplanation] = useState('');
-
-  useEffect(()=>{
-    setObjectList(dataSource.map((item) => item.key));
-  }, []);
 
   const handleFromDragStart = (e) => {
     e.dataTransfer.setData('value', e.target.innerText);
   };
+
   // 열,상수,연산자,함수 의 내용값 출력
   const handleObject = (e) => {
-    dataSource.forEach((item) => {
-      if (item.key === e.itemData) {
-        setSelectedObjectList(item.key);
-        setObjectListContents(item.collection.map((item) => item.key));
-      }
-    });
+    if (e.itemData) {
+      setSelectedObjectList(e.itemData.collection);
+    }
   };
+
   // 열,상수,연산자,함수 의 내용값 설명값 출력
   const handleObjectContent = (e) => {
-    dataSource.forEach((item) => {
-      if (item.key === selectedObjectList) {
-        item.collection.map((item) => {
-          if (item.key === e.itemData) {
-            setExplanation(item.explanation);
-          }
-        });
-      }
-    });
+    if (e.itemData) {
+      setExplanation(e.itemData.explanation);
+    }
   };
 
   return (
@@ -54,7 +39,9 @@ const CalcObjectList = () => {
             borderRadius: '8px',
             padding: '10px'
           }}
-          dataSource={objectList}
+          dataSource={dataSource}
+          displayExpr='key'
+          keyExpr='key'
           onItemClick={handleObject}
           width='100%'
           height='250px'>
@@ -68,9 +55,11 @@ const CalcObjectList = () => {
             borderRadius: '8px',
             padding: '10px'
           }}
-          dataSource={objectListContents}
+          dataSource={selectedObjectList}
           width='100%'
           height='250px'
+          displayExpr='key'
+          keyExpr='key'
           onItemClick={handleObjectContent}
           itemRender={(data) => {
             return (
@@ -78,7 +67,7 @@ const CalcObjectList = () => {
                 draggable={true}
                 onDragStart={handleFromDragStart}
               >
-                {data}
+                {data.key}
               </div>
             );
           }}>
