4fa8f9b1be2775c2f8d7fc370c0891c0bce70943
jins-sy <54256571+jin-sy@users.noreply.github.com>
2023-12-27 14:20:28 +0900

    feat: confgType 추가
    DesignerMode, RunMode

---
+++ src/main/view/src/components/config/configType.js
@@ -0,0 +1,15 @@
+const DesignerMode = {
+  DASHBOARD: 'dashboard',
+  ADHOC: 'adhoc',
+  SPREADSHEET: 'spreadSheet'
+};
+
+const RunMode = {
+  DESIGNER: 'designer',
+  VIEWER: 'viewer'
+};
+
+export {
+  DesignerMode,
+  RunMode
+};
