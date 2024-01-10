57047abea1536c098ec4a555ea5427c4c5460b86
송봉조 <103235327+Songbongjo@users.noreply.github.com>
2024-01-11 02:53:04 +0900

    파일 저장 중간저장

--- src/main/java/com/wise/MarketingPlatForm/fileUpload/controller/FileUploacController.java
+++ src/main/java/com/wise/MarketingPlatForm/fileUpload/controller/FileUploacController.java
@@ -1,20 +1,14 @@
 package com.wise.MarketingPlatForm.fileUpload.controller;
 
-import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.wise.MarketingPlatForm.fileUpload.service.FileUploadService;
 
 @RestController
@@ -23,6 +17,7 @@ public class FileUploacController {
 	
 	@Autowired
 	FileUploadService fileUploadService;
+	
 	@PostMapping("/import")
 	public void fileUpload(
 			HttpServletRequest httpServletRequest,
--- src/main/java/com/wise/MarketingPlatForm/fileUpload/service/FileUploadService.java
+++ src/main/java/com/wise/MarketingPlatForm/fileUpload/service/FileUploadService.java
@@ -1,31 +1,38 @@
 package com.wise.MarketingPlatForm.fileUpload.service;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
+	
 	@Value("${upload.path}")
 	private String uploadPath;
+	@Autowired
+	private ResourceLoader resourceLoader;
 	
 	public void saveFile(MultipartFile file, String fileName) {
         try (InputStream input = file.getInputStream()) {
-        	ClassLoader classLoader = getClass().getClassLoader();
-        	URL resourceURL = classLoader.getResource(uploadPath + fileName);
-        	if (resourceURL != null) {
-        	    Path filePath = Paths.get(resourceURL.toURI());
-                Files.createDirectories(filePath.getParent());
-                Files.copy(input, Paths.get(filePath.toString()), StandardCopyOption.REPLACE_EXISTING);
-        	} else {
-        	    System.out.println("리소스를 찾을 수 없습니다.");
+        	Resource folderResource = resourceLoader.getResource(uploadPath);
+        	if (!folderResource.exists()) {
+        		File uploadFolder = ResourceUtils.getFile(uploadPath);
+        		uploadFolder.mkdirs();
         	}
+        	Resource fileResource = resourceLoader.getResource(uploadPath + fileName);
+			Files.copy(input,
+				fileResource.getFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (Exception e) {
         	e.printStackTrace();
         } 
