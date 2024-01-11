a47b2e62fa6d08a5053739429edb8c95da2afb40
jins-sy <103473652+syjin2@users.noreply.github.com>
2024-01-11 21:26:09 +0900

    feat: 뷰어 보고서 불러오기 개발

--- src/main/view/src/components/common/atomic/ReportTab/organism/ReportTabs.jsx
+++ src/main/view/src/components/common/atomic/ReportTab/organism/ReportTabs.jsx
@@ -6,6 +6,9 @@ import refresh from '../../../../../assets/image/icon/button/refresh.png';
 
 import ReportListTab from './ReportListTab';
 import SmallImageButton from '../../Common/Button/SmallImageButton';
+import {useEffect, useState} from 'react';
+import {setIconReportList} from 'components/report/util/ReportUtility';
+import models from 'models';
 
 const ReportTabSource = [
   {
@@ -29,9 +32,19 @@ const ToolbarItems = [
 ];
 
 const ReportTabs = () => {
+  const [reportList, setReportList] = useState();
+
   const getTabContent = ({data}) => {
-    return <ReportListTab items={[]}/>;
+    return <ReportListTab items={reportList? reportList[data.id] : []}/>;
   };
+
+  useEffect(() => {
+    models.Report.getList('admin', null, 'viewer').then((data) => {
+      setIconReportList(data.privateReport);
+      setIconReportList(data.publicReport);
+      setReportList(data);
+    });
+  }, []);
 
   return (
     <Wrapper>
