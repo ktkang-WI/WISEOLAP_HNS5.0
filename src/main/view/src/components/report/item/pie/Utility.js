d000bc28f647a0f51943ab9c0b1d8455038b4f4d
YoungGiLee <yglee93@wise.co.kr>
2024-01-25 17:31:11 +0900

    불필요 코드 제거
    useButton: false 면 ...defaultMeasure 제외 필요 없음.

--- src/main/view/src/components/report/item/pie/Utility.js
+++ src/main/view/src/components/report/item/pie/Utility.js
@@ -50,13 +50,7 @@ const generateItem = () => {
  */
 const getDataFieldOptionChild = () => {
   const dataFieldMeasure = {
-    ...defaultMeasure,
-    useButton: false,
-    // 우측에 버튼 추가가 필요한 경우 사용하는 옵션 ex)시리즈 옵션
-    buttonIcon: '',
-    buttonEvent: function(e) {
-      console.log(e);
-    }
+    ...defaultMeasure
   };
 
   const dataFieldDimension = {
