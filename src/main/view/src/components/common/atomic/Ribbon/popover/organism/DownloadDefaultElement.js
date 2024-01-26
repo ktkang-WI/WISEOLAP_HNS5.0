c12b88f1a883810429217aac1bf6f460b22a0f08
mjhwang <mjhwang@wise.co.kr>
2024-01-26 13:09:30 +0900

    기능완성(위치이전 필요, 기능분리필요)

--- src/main/view/src/components/common/atomic/Ribbon/popover/organism/DownloadDefaultElement.js
+++ src/main/view/src/components/common/atomic/Ribbon/popover/organism/DownloadDefaultElement.js
@@ -4,7 +4,14 @@ import {useSelector} from 'react-redux';
 import {selectCurrentItems} from 'redux/selector/ItemSelector';
 import {selectRootParameter} from 'redux/selector/ParameterSelector';
 import {selectCurrentReport} from 'redux/selector/ReportSelector';
-import models from 'models';
+// import models from 'models';
+import PivotGrid from 'devextreme/ui/pivot_grid';
+import DataGrid from 'devextreme/ui/data_grid';
+import Chart from 'devextreme/viz/chart';
+import PieChart from 'devextreme/viz/pie_chart';
+import {Workbook} from 'exceljs';
+import saveAs from 'file-saver';
+import {exportPivotGrid, exportDataGrid} from 'devextreme/excel_exporter';
 
 const DownloadDefaultElement = () => {
   const items = selectCurrentItems(store.getState());
@@ -14,20 +21,106 @@ const DownloadDefaultElement = () => {
   const downloadAllSet = {items, parameters, dataSource};
   console.log('downloadAllSet', downloadAllSet);
 
-  function replacer(key, value) {
-    const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === 'object' && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
+  const addImageToWorksheet = async (workbook, worksheet, blob) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        const arrayBuffer = event.target.result;
+        const imageId = workbook.addImage({
+          buffer: arrayBuffer,
+          extension: 'png'
+        });
+        worksheet.addImage(imageId, {
+          tl: {col: 0.0, row: 0.0},
+          br: {col: 12.9999, row: 30.9999}
+        });
+        resolve();
+      };
+      reader.onerror = function(event) {
+        reject(event.target.error);
+      };
+      reader.readAsArrayBuffer(blob);
+    });
+  };
+  const handleDownload = async () => {
+    const workbook = new Workbook();
+    let worksheetCount = 0;
+    const elements = items.map((item) => {
+      const element = document.getElementById(item.id);
+      return {
+        type: item.type,
+        name: item.meta.name,
+        element: element
+      };
+    });
+    for (const elementObj of elements) {
+      if (elementObj.type === 'pivot') {
+        const instance = PivotGrid.getInstance(elementObj.element);
+        const worksheet =
+        workbook.addWorksheet(elementObj.name);
+        await exportPivotGrid({
+          component: instance,
+          worksheet: worksheet
+        });
+        worksheetCount++;
+      } else if (elementObj.type === 'grid') {
+        const instance = DataGrid.getInstance(elementObj.element);
+        const worksheet =
+        workbook.addWorksheet(elementObj.name);
+        await exportDataGrid({
+          component: instance,
+          worksheet: worksheet
+        });
+        worksheetCount++;
+      } else if (elementObj.type === 'chart') {
+        const instance = Chart.getInstance(elementObj.element);
+        const blob = await new Promise((resolve, reject) => {
+          instance.on('fileSaving', function(e) {
+            e.cancel = true; // Prevent default file saving
+            resolve(e.data); // Get the data URI of the chart image
+          });
+          instance.exportTo('PNG').catch(reject);
+        });
+        const worksheet = workbook.addWorksheet(elementObj.name);
+        await addImageToWorksheet(workbook, worksheet, blob);
+        worksheetCount++;
+      } else if (elementObj.type === 'pie') {
+        const instance = PieChart.getInstance(elementObj.element);
+        const blob = await new Promise((resolve, reject) => {
+          instance.on('fileSaving', function(e) {
+            e.cancel = true; // Prevent default file saving
+            resolve(e.data); // Get the data URI of the chart image
+          });
+          instance.exportTo('PNG').catch(reject);
+        });
+        const worksheet = workbook.addWorksheet(elementObj.name);
+        await addImageToWorksheet(workbook, worksheet, blob);
+        worksheetCount++;
       }
-      return value;
-    };
-  }
+    }
+    if (worksheetCount > 0) {
+      const blobType =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+      const buffer = await workbook.xlsx.writeBuffer();
+      const blob = new Blob([buffer], {type: blobType});
+      saveAs(blob, 'PivotGrids');
+    }
+  };
 
-  const safeStringify = (obj) => JSON.stringify(obj, replacer());
+  // function replacer(key, value) {
+  //   const seen = new WeakSet();
+  //   return (key, value) => {
+  //     if (typeof value === 'object' && value !== null) {
+  //       if (seen.has(value)) {
+  //         return;
+  //       }
+  //       seen.add(value);
+  //     }
+  //     return value;
+  //   };
+  // }
+
+  // const safeStringify = (obj) => JSON.stringify(obj, replacer());
   return {
     download: [
       {
@@ -38,8 +131,9 @@ const DownloadDefaultElement = () => {
             label: localizedString.excelXlsx,
             visible: true,
             onClick: () => {
-              models.Report.downloadReportAll(
-                  safeStringify(downloadAllSet), '.xlsx');
+              handleDownload();
+              // models.Report.downloadReportAll(
+              //     safeStringify(downloadAllSet), '.xlsx');
             }
           },
           {
