764ab9615879a2d7efaba6eda2a9e37ad46a32c4
jins-sy <103473652+syjin2@users.noreply.github.com>
2024-02-01 14:09:50 +0900

    refactor: 상수수정

--- src/main/view/src/components/config/organisms/configurationSetting/data/ConfigurationSettingData.js
+++ src/main/view/src/components/config/organisms/configurationSetting/data/ConfigurationSettingData.js
@@ -3,18 +3,18 @@ import GeneralConfigure from '../generalConfigure/GeneralConfigure';
 import ReportConfigure from '../reportConfigure/ReportConfigure';
 
 export const Mode = {
-  'GENERATECONFIGURE': 'GENERATECONFIGURE',
-  'REPORTCONFIGURE': 'REPORTCONFIGURE'
+  'GENERATE_CONFIGURE': 'GENERATE_CONFIGURE',
+  'REPORT_CONFIGURE': 'REPORT_CONFIGURE'
 };
 
 export const dataSource = [
   {
-    mode: Mode['GENERATECONFIGURE'],
+    mode: Mode['GENERATE_CONFIGURE'],
     title: localizedString.generateConfigure,
     component: <GeneralConfigure/>
   },
   {
-    mode: Mode['REPORTCONFIGURE'],
+    mode: Mode['REPORT_CONFIGURE'],
     title: localizedString.reportConfigure,
     component: <ReportConfigure/>
   }
