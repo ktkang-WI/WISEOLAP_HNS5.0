297c6897541f5da15d7cf434e1505fce5b3a0899
wise-ajkim <99698967+wise-ajkim@users.noreply.github.com>
2024-02-02 15:30:04 +0900

    fix: WebConfig 수정

--- src/main/java/com/wise/MarketingPlatForm/global/config/WebConfig.java
+++ src/main/java/com/wise/MarketingPlatForm/global/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
             .addPathPatterns("/**")
-            .excludePathPatterns("/login/**", "/error", "/", "/js/**",
+            .excludePathPatterns("/login/**", "/error", "/", "/js/**", "/static/**",
                     "/css/**", "/images/**", "/favicon.ico", "/index.html");
     }
 
