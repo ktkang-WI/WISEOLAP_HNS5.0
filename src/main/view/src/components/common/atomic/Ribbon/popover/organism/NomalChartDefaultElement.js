cb136b1ee5dbcd91a2fee4184ef55ad49f86a958
mjhwang <mjhwang@wise.co.kr>
2024-02-06 16:49:51 +0900

    메인추가

--- src/main/view/src/components/common/atomic/Ribbon/popover/organism/NomalChartDefaultElement.js
+++ src/main/view/src/components/common/atomic/Ribbon/popover/organism/NomalChartDefaultElement.js
@@ -3,71 +3,137 @@ import useLayout from 'hooks/useLayout';
 import usePopover from 'hooks/usePopover';
 import {useSelector} from 'react-redux';
 import {selectCurrentReportId} from 'redux/selector/ReportSelector';
-import bar from '../../../../../../assets/image/icon/button/series_type.png';
-import pie from '../../../../../../assets/image/icon/button/pieChart.png';
+import pie from 'assets/image/icon/button/pieChart.png';
+import {chartImages} from 'components/report/item/util/chartImageImporter';
+import {chartItemType} from 'components/report/item/chart/chartItemType';
 
-const NomalChartDefaultElement = () => {
+const NormalChartDefaultElement = () => {
+  // hook
   const {insertFlexLayout} = useLayout();
   const {closePopover} = usePopover();
+  // state
   const selectedReportId = useSelector(selectCurrentReportId);
-  return {
+  const chart = 'chart';
+  const onClickChartItem = (selectedReportId, chart, chartType) => {
+    insertFlexLayout(selectedReportId, chart, chartType);
+    closePopover();
+  };
+  const normalChartElemet = {
     barChart: [
       {
-        imgSrc: bar,
+        imgSrc: chartImages[chartItemType.bar],
         label: localizedString.barChart,
-        onClick: () => {
-          insertFlexLayout(selectedReportId, 'chart');
-          closePopover();
-        }
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.bar)
       },
       {
-        imgSrc: bar,
-        label: 'aaa',
-        onClick: () => {
-        }
+        imgSrc: chartImages[chartItemType.stackedbar],
+        label: localizedString.stackedBarChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.stackedbar)
       },
       {
-        imgSrc: bar,
-        label: 'ccc'
-      },
-      {
-        imgSrc: bar,
-        label: 'dddd'
+        imgSrc: chartImages[chartItemType.fullstackedbar],
+        label: localizedString.fullStackedBarChart,
+        onClick: () =>
+          onClickChartItem(
+              selectedReportId, chart, chartItemType.fullstackedbar)
       }
     ],
     lineChart: [
       {
-        imgSrc: bar,
-        label: 'ddd'
+        imgSrc: chartImages[chartItemType.scatter],
+        label: localizedString.scatterChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.scatter)
       },
       {
-        imgSrc: bar,
-        label: 'aaa'
+        imgSrc: chartImages[chartItemType.line],
+        label: localizedString.lineChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.line)
       },
       {
-        imgSrc: bar,
-        label: 'ccc'
+        imgSrc: chartImages[chartItemType.stackedline],
+        label: localizedString.stackedLineChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.stackedline)
+      },
+      {
+        imgSrc: chartImages[chartItemType.fullstackedline],
+        label: localizedString.fullStackedLineChart,
+        onClick: () =>
+          onClickChartItem(
+              selectedReportId, chart, chartItemType.fullstackedline)
+      },
+      {
+        imgSrc: chartImages[chartItemType.stepline],
+        label: localizedString.stepLineChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.stepline)
+      },
+      {
+        imgSrc: chartImages[chartItemType.spline],
+        label: localizedString.splineChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.spline)
       }
     ],
     areaChart: [
       {
-        imgSrc: bar,
-        label: 'ddd'
+        imgSrc: chartImages[chartItemType.area],
+        label: localizedString.stackChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.area)
       },
       {
-        imgSrc: bar,
-        label: 'aaa'
+        imgSrc: chartImages[chartItemType.stackedarea],
+        label: localizedString.stackedAreaChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.stackedarea)
       },
       {
-        imgSrc: bar,
-        label: 'ccc'
+        imgSrc: chartImages[chartItemType.fullstackedarea],
+        label: localizedString.fullStackedAreaChart,
+        onClick: () =>
+          onClickChartItem(
+              selectedReportId, chart, chartItemType.fullstackedarea)
+      },
+      {
+        imgSrc: chartImages[chartItemType.steparea],
+        label: localizedString.stepAreaChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.steparea)
+      },
+      {
+        imgSrc: chartImages[chartItemType.splinearea],
+        label: localizedString.splineAreaChart,
+        onClick: () =>
+          onClickChartItem(selectedReportId, chart, chartItemType.splinearea)
+      },
+      {
+        imgSrc: chartImages[chartItemType.stackedsplinearea],
+        label: localizedString.stackedSplineAreaChart,
+        onClick: () =>
+          onClickChartItem(
+              selectedReportId, chart, chartItemType.stackedsplinearea)
+      },
+      {
+        imgSrc: chartImages[chartItemType.fullstackedsplinearea],
+        label: localizedString.fullStackedSplineAreaChart,
+        onClick: () =>
+          onClickChartItem(
+              selectedReportId, chart, chartItemType.fullstackedsplinearea)
       }
     ],
     restChart: [
-      {
-        imgSrc: bar,
-        label: 'ddd'
-      },
+      // TODO: bubble chart 추가
+      // {
+      //   imgSrc: chartImages[chartItemType.bubble],
+      //   label: localizedString.BubbleChart,
+      //   onClick: ()
+      //   => onClickChartItem(selectedReportId, chart, chartItemType.bubble)
+      // },
       {
         imgSrc: pie,
         label: localizedString.pieChart,
@@ -75,10 +141,6 @@ const NomalChartDefaultElement = () => {
           insertFlexLayout(selectedReportId, 'pie');
           closePopover();
         }
-      },
-      {
-        imgSrc: bar,
-        label: 'ccc'
       }
     ],
     keys: [
@@ -88,5 +150,6 @@ const NomalChartDefaultElement = () => {
       'restChart'
     ]
   };
+  return normalChartElemet;
 };
-export default NomalChartDefaultElement;
+export default NormalChartDefaultElement;
