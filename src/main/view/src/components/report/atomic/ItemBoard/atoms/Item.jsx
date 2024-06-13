ba945ffc6ae5a11946283f095e7ee59991d809d6
김아진 <99698967+wise-ajkim@users.noreply.github.com>
2024-06-13 10:11:41 +0900

    fix: contextMenu 아이템 영역 밖에서도 작동하는 오류 수정

--- src/main/view/src/components/report/atomic/ItemBoard/atoms/Item.jsx
+++ src/main/view/src/components/report/atomic/ItemBoard/atoms/Item.jsx
@@ -3,11 +3,9 @@ import {styled} from 'styled-components';
 import {getTheme} from 'config/theme';
 import ItemType from 'components/report/item/util/ItemType';
 import ScrollSetting from 'components/report/util/ScrollSetting';
-import {useEffect, useRef} from 'react';
+import {useRef} from 'react';
 import {ContextMenu} from 'devextreme-react';
 import useContextMenu from 'hooks/useContextMenu';
-import {selectLinkedReport} from 'redux/selector/LinkSelector';
-import {useSelector} from 'react-redux';
 
 const theme = getTheme();
 
@@ -26,57 +24,29 @@ const ItemContent = styled.div`
   padding: 10px 20px;
 `;
 
-// TODO: 추후 아이템별로 나눠야함.
 const Item = ({children, item, ...props}) => {
   const {className, overflowY} = ScrollSetting.getScrollOptions({
     overflowYItems: [ItemType.COLLAPSIBLE_TREE],
     type: children?.type?.type?.name
   });
 
-  const targetRef = useRef();
   const contextMenuRef = useRef();
-  const linkedReport = useSelector(selectLinkedReport);
   const {getContextMenuItems} = useContextMenu(item);
 
-  // context menu 등록
-  useEffect(() => {
-    if (
-      !item ||
-      item.type == ItemType.PIVOT_GRID ||
-      item.type == ItemType.DATA_GRID
-    ) return;
-    const handleContextMenu = (e) => {
-      e.preventDefault();
-      if (contextMenuRef.current) {
-        contextMenuRef.current.instance.option('items',
-            getContextMenuItems());
-        contextMenuRef.current.instance.show(e.clientX, e.clientY);
-      }
-    };
-
-    const targetElement = targetRef.current;
-
-    if (targetElement) {
-      targetElement.addEventListener('contextmenu', handleContextMenu);
-    }
-
-    // Clean up the event listener on component unmount
-    return () => {
-      if (targetElement) {
-        targetElement.removeEventListener('contextmenu', handleContextMenu);
-      }
-    };
-  }, [item, linkedReport]);
-
   return (
-    <ItemWrapper ref={targetRef} className={className} overflowY={overflowY}>
+    <ItemWrapper
+      id={item.id + '_wrapper'}
+      className={className}
+      overflowY={overflowY}>
+      <ItemContent>{children}</ItemContent>
       <ContextMenu
+        items={getContextMenuItems()}
         onItemClick={(e) => {
           e.itemData?.onItemClick();
         }}
+        target={'#' + item.id + '_wrapper'}
         ref={contextMenuRef}
       />
-      <ItemContent>{children}</ItemContent>
     </ItemWrapper>
   );
 };
