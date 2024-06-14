eec33831942b8beea9c9ab91ebdae584dd3bb671
진승연 <103473652+syjin2@users.noreply.github.com>
2024-06-14 18:20:48 +0900

    hotfix: 로컬 개발용 라이센스 남기도록 수정 (잘못커밋)

--- src/main/view/src/components/report/atomic/spreadBoard/util/SpreadCore.js
+++ src/main/view/src/components/report/atomic/spreadBoard/util/SpreadCore.js
@@ -17,9 +17,34 @@ GC.Spread.Common.CultureManager.culture('ko-kr');
  * @param {String} spreadJsDesignLicense
  * @param {String} spreadJsLicense
  */
-export const setSpreadLicense = async (
+export const setSpreadLicense = (
     spreadJsDesignLicense, spreadJsLicense) => {
   // Spread 16v TEST 용
+  spreadJsDesignLicense = 'intelligence.wise.co.kr,E667286354186462#B1t4DnZ' +
+  '6T6tCbZdlVLhkch3EcNZkSwQmeY3EWVNFUxZHNxRGe8gEOwMndr9ES9BVY4FVcYVESFtiV0BH' +
+  'MudmdwhVUBRDOwdHZoBlZvg7VmlXMvxmRYFUOVZ6VzhncqNnTsRUb4JFRvUnQNB5T4F4Slhkey' +
+  'Y4KhtiR8d5LsplcnFTSO3CT8IGMvZFSsl6V7dFW8pmbJVFUyM7Z7plcwYVNxMHT43SdDBzSnRD' +
+  'WkFTcityZ7VTOnhXbM3iS8FnePZVNJh5bKVHdXtURvZlbsRzbKpVY9wEVMNzbGdmMiZmMsxURn' +
+  'ZVNolUcEhzK0d7YHd4NZZ6RO9WeZNGZsxEO0JzdJNmNVxmZvR5V9cTaEp7RwxWWiojITJCLiUE' +
+  'N6ITOGhjI0ICSiwCNzMTO5UTOxAjM0IicfJye&Qf35Vfi4EU9cjI0IyQiwiI6EjL6BibvRGZB5i'+
+  'cl96ZpNXZE5yUKRWYlJHcTJiOi8kI1tlOiQmcQJCLicDNzMzNwASMyMDM4IDMyIiOiQncDJCLi' +
+  'kTM6ADNyAjMiojIwhXRiwiIytmLvNmLlNXa79SZj9WZnlGbsVGdulmI0IyctRkIsISjF6OtdyOh'+
+  'VyOuEyOhcyuI0ISYONkIsUWdyRnOiwmdFJCLiIjN4YDOxQTNzYDOycjN6IiOiQWSiwSfdJSZsJ' +
+  'WYUR7b6lGUislOicGbmJCLlNHbhZmOiI7ckJye0ICbuFkI1pjIEJCLi4TPRR5NihVWz3CcKty'+
+  'V4okdKhkT6pEUulzaERUMYB5S8lHMs5UZ6RDRysGWZBlZyYHTyV6LWhnZa36MtlnRKlFUyBn'+
+  'Qwumqv';
+  spreadJsLicense = 'intelligence.wise.co.kr,E435423422424783#B1lL6VFWv4UeKV'+
+  'zVzFTaQl5VrEHaQ56KZdmc4V5TsN7ZB5WM9V4cmFVUzlla4BTMa94Mn9kNrtyK4cXUy2GRXxWRG'+
+  'hnUtp6RldlZZNzUZN4L4lmQ83kWvZWWJdza4NzLhlTOldmdJdTSQpXexdkZFZmQ5VlZ6ZXbzV'+
+  '7LwMHeOtiQadjW53STwoXbYhVaV3CM5UHZFpUUpZmNVZXN9AjRuV5NlZWVoJFdE3SWINXZKdz'+
+  'LzsWVS36dNFXQEV5Sr9kQ5JVWjh4VEp7K9UjUpRUeUVzV6Y6arlnWElnM4Qza7knVQF5TvN6b'+
+  'RpFSWt6ap3WZlhHSWN7Vy5UO5RkQuJTUVp6QN3CTyNTbrdUSiojITJCLiYjRxAzNFVkI0ICSi'+
+  'wSN7QTMwkDO5QTM0IicfJye#4Xfd5nIXR5QaJiOiMkIsIiNx8idgMlSgQWYlJHcTJiOi8kI1'+
+  'tlOiQmcQJCLicTMzMzNwASMyMDM4IDMyIiOiQncDJCLikTM6ADNyAjMiojIwhXRiwiIytmLvN'+
+  'mLlNXa79SZj9WZnlGbsVGdulmI0IyctRkIsISjF6OtdyOhVyOuEyOhcyuI0ISYONkIsUWdyRn'+
+  'OiwmdFJCLiMDO7QjM4IjM4MjM4UzM4IiOiQWSiwSfdJSZsJWYUR7b6lGUislOicGbmJCLlNH'+
+  'bhZmOiI7ckJye0ICbuFkI1pjIEJCLi4TPRhTNTVDTG9GZuREcDpnaydWSsh5V4cGRiFzRxME'+
+  'UrYUb7JFMjhWWElEVO3UY7UVcqtUV4IWM4NHaLxkWwB7NWZGMQNzVD3WOxM1RJl';
   if (process.env.NODE_ENV != 'development') {
     GC.Spread.Sheets.LicenseKey = spreadJsLicense;
     ExcelIO.LicenseKey = spreadJsLicense;
