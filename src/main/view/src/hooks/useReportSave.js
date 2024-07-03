24607ac942e72f9eb990e0981dd88e2b8632f860
진승연 <103473652+syjin2@users.noreply.github.com>
2024-07-03 09:55:11 +0900

    fix: alert localization 추가

--- src/main/view/src/hooks/useReportSave.js
+++ src/main/view/src/hooks/useReportSave.js
@@ -264,7 +264,7 @@ const useReportSave = () => {
 
       if (reportBos.chkGb.length > 0) {
         // eslint-disable-next-line max-len
-        alert('보고서의 데이터가 BOS 와 일치하지 않습니다.\n\n' + 'code: ' + reportBos.chkGb);
+        alert(localizedString.checkDwReport + reportBos.chkGb);
       }
     } catch (error) {
       new Error('Report load Error');
