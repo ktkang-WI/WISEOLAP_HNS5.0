0bb660fcd999115900f60641c28ed94b25a31479
hoihufu <103414954+hoihufu@users.noreply.github.com>
2024-09-10 16:16:23 +0900

    서브연결보고서 임시 주석처리 및 컬럼정보 바꾸기 저장시 오류수정

--- src/main/view/src/components/report/atomic/LinkReport/molecules/LinkParamInfo.jsx
+++ src/main/view/src/components/report/atomic/LinkReport/molecules/LinkParamInfo.jsx
@@ -12,16 +12,16 @@ import LinkSlice from 'redux/modules/LinkSlice';
 import {
   selectCurrentReport
 } from 'redux/selector/ReportSelector';
-import {selectCurrentDesignerMode} from 'redux/selector/ConfigSelector';
+// import {selectCurrentDesignerMode} from 'redux/selector/ConfigSelector';
 import {selectLinkedReport} from 'redux/selector/LinkSelector';
 import {
   // processSubLinkParamData,
-  processLinkParamData,
-  createLinkDataXML
+  processLinkParamData
+  // createLinkDataXML
 }
   from 'components/report/atomic/LinkReport/molecules/LinkReportFunction';
-import {selectSelectedItemId, selectCurrentItemType}
-  from 'redux/selector/ItemSelector';
+// import {selectSelectedItemId, selectCurrentItemType}
+//   from 'redux/selector/ItemSelector';
 import {useSelector} from 'react-redux';
 import {selectCurrentInformationas} from 'redux/selector/ParameterSelector';
 // import _ from 'lodash';
@@ -74,31 +74,33 @@ const LinkParamInfo = ({
   selectedRowData,
   linkParamData,
   onClose,
-  paramInfo,
-  setParamInfo,
-  subYn,
-  subLinkDim,
-  subLinkParamInfo,
-  setSubLinkParamInfo,
+  // paramInfo,
+  // setParamInfo,
+  // subYn,
+  // subLinkDim,
+  // subLinkParamInfo,
+  // setSubLinkParamInfo,
   linkFkInfo,
-  setLinkFkInfo,
-  subLinkFkInfo,
-  setSubLinkFkInfo
+  setLinkFkInfo
+  // subLinkFkInfo,
+  // setSubLinkFkInfo
 }) => {
   const [fkNmOptions, setFkNmOptions] = useState([]);
-  const [subFkNmOptions, setSubFkNmOptions] = useState([]);
+  // const [subFkNmOptions, setSubFkNmOptions] = useState([]);
   const dispatch = useDispatch();
   const {insertLink, updateLink} = LinkSlice.actions;
   const prevLinkParamData = usePrevious(linkParamData?.reports[0]?.reportId);
   const selectLinkedReportList = useSelector(selectLinkedReport);
   // (store.getState());
-  const focusedItemId = useSelector(selectSelectedItemId);
-  const focusedItemType = useSelector(selectCurrentItemType);
+  // const focusedItemId = useSelector(selectSelectedItemId);
+  // const focusedItemType = useSelector(selectCurrentItemType);
   const currentItemParam = useSelector(selectCurrentInformationas);
   const currentReport = useSelector(selectCurrentReport);
-  const currentReportType = useSelector(selectCurrentDesignerMode);
-  const [clonedParamInfo, setClonedParamInfo] = useState([]);
-  const [clonedFkNmOptions, setClonedFkNmOptions] = useState([]);
+  // const currentReportType = useSelector(selectCurrentDesignerMode);
+  // const [clonedParamInfo, setClonedParamInfo] = useState([]);
+  // const [clonedFkNmOptions, setClonedFkNmOptions] = useState([]);
+  const [paramInfo, setParamInfo] = useState([]);
+  // const newParamInfo = _.cloneDeep(paramInfo);
 
   useEffect(() => {
     if (linkParamData &&
@@ -110,11 +112,11 @@ const LinkParamInfo = ({
             setFkNmOptions,
             setLinkFkInfo,
             currentItemParam,
-            subLinkParamInfo,
-            setSubLinkParamInfo,
-            setSubFkNmOptions,
-            subLinkDim,
-            subYn
+            // subLinkParamInfo,
+            // setSubLinkParamInfo,
+            // setSubFkNmOptions,
+            // subLinkDim,
+            // subYn
         );
       } else {
         processLinkParamData(
@@ -123,91 +125,93 @@ const LinkParamInfo = ({
             setFkNmOptions,
             setLinkFkInfo,
             currentItemParam,
-            subLinkParamInfo,
-            setSubLinkParamInfo,
-            setSubFkNmOptions,
-            subLinkDim,
-            subYn
+            // subLinkParamInfo,
+            // setSubLinkParamInfo,
+            // setSubFkNmOptions,
+            // subLinkDim,
+            // subYn
         );
       }
     }
-    const deepClonedParamInfo = _.cloneDeep(paramInfo);
-    setClonedParamInfo(deepClonedParamInfo);
-    const deepClonedFkNmOptions = _.cloneDeep(fkNmOptions);
-    setClonedFkNmOptions(deepClonedFkNmOptions);
+    // const deepClonedParamInfo = _.cloneDeep(paramInfo);
+    // setClonedParamInfo(deepClonedParamInfo);
+    // const deepClonedFkNmOptions = _.cloneDeep(fkNmOptions);
+    // setClonedFkNmOptions(deepClonedFkNmOptions);
   }, [
     linkParamData,
     prevLinkParamData,
     setParamInfo,
     setLinkFkInfo,
     setFkNmOptions,
-    setSubLinkParamInfo,
+    // setSubLinkParamInfo,
     paramInfo
   ]
   );
-  console.log('clonedParamInfo:', clonedParamInfo);
+  // console.log('clonedParamInfo:', clonedParamInfo);
 
-  const subLinkInfo = {
-    reportId: 0,
-    linkReportId: 0,
-    subLinkItemId: '',
-    dataLinkType: 'LP',
-    subLinkXmlParam: '',
-    subLinkXmlData: '',
-    subLinkReportNm: '',
-    subLinkReportOrdinal: 0,
-    subLinkReportType: '',
-    subLinkParamInfo: []
-  };
+  // const subLinkInfo = {
+  //   reportId: 0,
+  //   linkReportId: 0,
+  //   subLinkItemId: '',
+  //   dataLinkType: 'LP',
+  //   subLinkXmlParam: '',
+  //   subLinkXmlData: '',
+  //   subLinkReportNm: '',
+  //   subLinkReportOrdinal: 0,
+  //   subLinkReportType: '',
+  //   subLinkParamInfo: []
+  // };
   const linkReportInfo = {
     reportId: 0,
+    reportNm: '',
+    reportType: '',
     linkReportId: 0,
-    linkXmlParam: '',
-    linkReportOrdinal: 0,
-    linkReportType: '',
-    dataLinkType: 'LP',
     linkReportNm: '',
+    linkReportType: '',
     linkParamInfo: [],
-    linkFkInfo: [],
+    linkFkInfo: []
+    // linkXmlParam: '',
+    // linkReportOrdinal: 0,
+    // dataLinkType: 'LP',
     // subLinkReport Parameter Point
-    subYn: 'False',
-    subLinkReport: subLinkInfo
+    // subYn: 'False',
+    // subLinkReport: subLinkInfo
   };
 
   const confirm = () => {
     const linkReportId = linkParamData.reports[0].reportId;
-    const dataPairs = paramInfo.map((info) => ({
-      FK_COL_NM: info.fkParam,
-      PK_COL_NM: info.pkParam
-    }));
-    const linkXmlParam = createLinkDataXML(dataPairs);
-    let subDataPairs;
-    if (subYn) {
-      subDataPairs = subLinkParamInfo.map((info) => ({
-        FK_COL_NM: info.fkParam === undefined ? 'None' : info.fkParam,
-        PK_COL_NM: info.pkParam
-      }));
-    }
+    // const dataPairs = paramInfo.map((info) => ({
+    //   FK_COL_NM: info.fkParam,
+    //   PK_COL_NM: info.pkParam
+    // }));
+    // const linkXmlParam = createLinkDataXML(dataPairs);
+    // let subDataPairs;
+    // if (subYn) {
+    //   subDataPairs = subLinkParamInfo.map((info) => ({
+    //     FK_COL_NM: info.fkParam === undefined ? 'None' : info.fkParam,
+    //     PK_COL_NM: info.pkParam
+    //   }));
+    // }
 
     linkReportInfo.reportId = currentReport.reportId;
     linkReportInfo.reportNm = currentReport.options.reportNm;
+    linkReportInfo.reportType = currentReport.options.reportType;
     linkReportInfo.linkReportId = linkReportId;
     linkReportInfo.linkReportNm = selectedRowData.name;
     linkReportInfo.linkReportType = selectedRowData.reportType;
-    linkReportInfo.linkXmlParam = linkXmlParam;
-    linkReportInfo.reportType = currentReport.options.reportType;
+    // linkReportInfo.linkXmlParam = linkXmlParam;
     linkReportInfo.linkParamInfo = paramInfo;
     linkReportInfo.linkFkInfo = linkFkInfo;
-    if (subYn) {
-      linkReportInfo.subYn = 'True';
-      subLinkInfo.reportId = currentReport.reportId;
-      subLinkInfo.linkReportId = linkReportId;
-      subLinkInfo.subLinkItemId = focusedItemType + focusedItemId;
-      subLinkInfo.subLinkXmlParam = linkXmlParam;
-      subLinkInfo.subLinkXmlData = createLinkDataXML(subDataPairs);
-      subLinkInfo.subLinkReportType = currentReportType;
-      subLinkInfo.subLinkParamInfo = subLinkParamInfo;
-    }
+    // if (subYn) {
+    //   linkReportInfo.subYn = 'True';
+    //   subLinkInfo.reportId = currentReport.reportId;
+    //   subLinkInfo.linkReportId = linkReportId;
+    //   subLinkInfo.subLinkItemId = focusedItemType + focusedItemId;
+    //   subLinkInfo.subLinkXmlParam = linkXmlParam;
+    //   subLinkInfo.subLinkXmlData = createLinkDataXML(subDataPairs);
+    //   subLinkInfo.subLinkReportType = currentReportType;
+    //   subLinkInfo.subLinkParamInfo = subLinkParamInfo;
+    // }
     const immutableLinkReportInfo = JSON.parse(JSON.stringify(linkReportInfo));
     if (!selectLinkedReportList.hasOwnProperty(linkReportId)) {
       dispatch(insertLink(immutableLinkReportInfo));
@@ -225,8 +229,9 @@ const LinkParamInfo = ({
           매개변수 매핑 정보
         </StyledTitle>
         <DevDataGrid
-          height={subYn ? '43%' : '76%'}
-          dataSource={paramInfo}
+          // height={subYn ? '43%' : '76%'}
+          height={'43%'}
+          dataSource={JSON.parse(JSON.stringify(paramInfo))}
           showBorders={true}
           showRowLines={true}
           showColumnLines={true}
@@ -236,15 +241,17 @@ const LinkParamInfo = ({
             allowUpdating: true
           }}
           onRowUpdated={(e) => {
-            const newData = clonedParamInfo.map((item) => {
+            console.log('e:', e);
+            console.log('paramInfo:', paramInfo);
+            const newData = paramInfo.map((item) => {
               if (item.pkNm === e.key.pkNm) {
-                const selectedOption = clonedFkNmOptions.find(
-                    (option) => option.fkNm === e.data.fkNm
+                const selectedOption = fkNmOptions.find(
+                    (option) => option.CfkNm === e.data.MfkNm
                 );
                 return {
                   ...item,
-                  fkNm:
-                    selectedOption ? selectedOption.fkNm : item.fkNm,
+                  MfkNm:
+                    selectedOption ? selectedOption.CfkNm : item.MfkNm,
                   fkParam:
                     selectedOption ? selectedOption.fkParam : item.fkParam
                 };
@@ -260,19 +267,23 @@ const LinkParamInfo = ({
             allowEditing={false}
           />
           <Column
-            dataField="fkNm"
+            dataField="MfkNm"
             caption="대상 보고서 매개변수"
             lookup={
               {
                 dataSource: fkNmOptions,
-                valueExpr: 'fkNm',
-                displayExpr: 'caption'
+                valueExpr: 'CfkNm',
+                displayExpr: 'caption',
+                calculateCellValue: (data) => {
+                  console.log('data:', data);
+                  return data;
+                }
               }
             }
             allowEditing={true}
           />
         </DevDataGrid>
-        {subYn && (
+        {/* {subYn && (
           <>
             <StyledTitle marginTop="14px">
               차원 데이터 연결
@@ -325,7 +336,7 @@ const LinkParamInfo = ({
               />
             </DevDataGrid>
           </>
-        )}
+        )} */}
       </StyledWrapper>
       <Footer>
         <CommonButton type='primary' maxWidth='120px' onClick={confirm}>
