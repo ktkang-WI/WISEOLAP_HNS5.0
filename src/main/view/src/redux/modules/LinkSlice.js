09d2e0ef0ad9793dd3e0e9ee7e0c3371e9aa26e3
hoihufu <103414954+hoihufu@users.noreply.github.com>
2024-09-10 16:13:05 +0900

    서브연결 임시 주석

--- src/main/view/src/redux/modules/LinkSlice.js
+++ src/main/view/src/redux/modules/LinkSlice.js
@@ -8,69 +8,83 @@ const initialState = {
 const reducers = {
   initLink: () => initialState,
   insertLink(state, action) {
-    const {linkReportId, subYn, subLinkReport} = action.payload;
+    const {
+      linkReportId
+      // subYn,
+      // subLinkReport
+    } = action.payload;
     if (!state.linkReport[linkReportId]) {
       state.linkReport[linkReportId] =
         {
-          ...action.payload,
-          subLinkReport: subYn === 'True' ?
-            [subLinkReport].flat() : []
+          ...action.payload
+          // subLinkReport: subYn === 'True' ?
+          //   [subLinkReport].flat() : []
         };
     } else {
-      if (subYn === 'True') {
-        const newSubLinks =
-          Array.isArray(subLinkReport) ? subLinkReport : [subLinkReport];
-        newSubLinks.forEach((newSubLink) => {
-          const index =
-            state.linkReport[linkReportId].subLinkReport.findIndex(
-                (sub) => sub.subLinkItemId === newSubLink.subLinkItemId
-            );
-          if (index !== -1) {
-            state.linkReport[linkReportId].subLinkReport[index] = newSubLink;
-          } else {
-            state.linkReport[linkReportId].subLinkReport.push(newSubLink);
-          }
-        });
-      } else {
-        state.linkReport[linkReportId] = {
-          ...action.payload,
-          subLinkReport: state.linkReport[linkReportId].subLinkReport
-        };
-      }
+      state.linkReport[linkReportId] = {
+        ...action.payload
+      };
+      // if (subYn === 'True') {
+      // const newSubLinks =
+      //   Array.isArray(subLinkReport) ? subLinkReport : [subLinkReport];
+      // newSubLinks.forEach((newSubLink) => {
+      //   const index =
+      //     state.linkReport[linkReportId].subLinkReport.findIndex(
+      //         (sub) => sub.subLinkItemId === newSubLink.subLinkItemId
+      //     );
+      //   if (index !== -1) {
+      //     state.linkReport[linkReportId].subLinkReport[index] = newSubLink;
+      //   } else {
+      //     state.linkReport[linkReportId].subLinkReport.push(newSubLink);
+      //   }
+      // });
+      // } else {
+      // subLinkReport: state.linkReport[linkReportId].subLinkReport
+      // };
     }
   },
 
   updateLink(state, action) {
-    const {linkReportId, subYn, subLinkReport} = action.payload;
-    if (subYn === 'True' && state.linkReport[linkReportId]) {
-      const newSubLinks =
-        Array.isArray(subLinkReport) ? subLinkReport : [subLinkReport];
-      newSubLinks.forEach((newSubLink) => {
-        const index =
-          state.linkReport[linkReportId].subLinkReport.findIndex(
-              (sub) => sub.subLinkItemId === newSubLink.subLinkItemId);
-        if (index !== -1) {
-          state.linkReport[linkReportId].subLinkReport[index] = newSubLink;
-        } else {
-          state.linkReport[linkReportId].subLinkReport.push(newSubLink);
-        }
-      });
+    const {
+      linkReportId
+      // subYn,
+      // subLinkReport
+    } = action.payload;
+    // subYn === 'True' &&
+    if (state.linkReport[linkReportId]) {
+      // const newSubLinks =
+      //   Array.isArray(subLinkReport) ? subLinkReport : [subLinkReport];
+      // newSubLinks.forEach((newSubLink) => {
+      //   const index =
+      //     state.linkReport[linkReportId].subLinkReport.findIndex(
+      //         (sub) => sub.subLinkItemId === newSubLink.subLinkItemId);
+      //   if (index !== -1) {
+      //     state.linkReport[linkReportId].subLinkReport[index] = newSubLink;
+      //   } else {
+      //     state.linkReport[linkReportId].subLinkReport.push(newSubLink);
+      //   }
+      // });
     } else {
       state.linkReport[linkReportId] =
         {
-          ...action.payload,
-          subLinkReport: state.linkReport[linkReportId]?.subLinkReport || []
+          ...action.payload
+          // subLinkReport: state.linkReport[linkReportId]?.subLinkReport || []
         };
     }
   },
 
   deleteLink(state, action) {
-    const {linkReportId, subYn, subLinkItemId} = action.payload;
-    if (subYn === 'True' && state.linkReport[linkReportId]) {
-      state.linkReport[linkReportId].subLinkReport =
-        state.linkReport[linkReportId].subLinkReport.filter(
-            (sub) => sub.subLinkItemId !== subLinkItemId
-        );
+    const {
+      linkReportId
+      // subYn,
+      // subLinkItemId
+    } = action.payload;
+    // subYn === 'True' &&
+    if (state.linkReport[linkReportId]) {
+      // state.linkReport[linkReportId].subLinkReport =
+      //   state.linkReport[linkReportId].subLinkReport.filter(
+      //       (sub) => sub.subLinkItemId !== subLinkItemId
+      //   );
     } else {
       delete state.linkReport[linkReportId];
     }
@@ -81,51 +95,55 @@ const reducers = {
       state.linkReport = {};
       return;
     }
-    const {linkReportId} = action.payload;
-    state.linkReport[linkReportId] = action.payload;
-  },
-
-  setSubLinkReport(state, action) {
-    if (!action.payload) return;
-    state.linkReport = {};
-    const {
-      reportId,
-      linkReportId,
-      linkSubItem,
-      linkDataType,
-      linkXmlParam,
-      linkXmlData,
-      linkReportOrdinal,
-      linkReportType,
-      linkReportNm
-    } = action.payload;
-    const matchSubLinkInfo ={
-      reportId: reportId,
-      linkReportId: linkReportId,
-      subLinkItemId: linkSubItem,
-      dataLinkType: linkDataType,
-      subLinkXmlParam: linkXmlParam,
-      subLinkXmlData: linkXmlData,
-      subLinkReportOrdinal: linkReportOrdinal,
-      subLinkReportType: linkReportType,
-      sublinkReportNm: linkReportNm,
-      subLinkParamInfo: []
-    };
-    const matchLinkInfo = {
-      reportId: reportId,
-      linkReportId: linkReportId,
-      linkXmlParam: linkXmlParam,
-      linkReportOrdinal: linkReportOrdinal,
-      linkReportType: linkReportType,
-      dataLinkType: linkDataType,
-      linkReportNm: linkReportNm,
-      linkParamInfo: [],
-      linkFkInfo: [],
-      subYn: 'True',
-      subLinkReport: matchSubLinkInfo
-    };
-    state.linkReport[linkReportId] = matchLinkInfo;
+    const reports =
+      Array.isArray(action.payload) ? action.payload : [action.payload];
+    reports.forEach((report) => {
+      const {linkReportId} = report;
+      state.linkReport[linkReportId] = report;
+    });
   }
+
+  // setSubLinkReport(state, action) {
+  //   if (!action.payload) return;
+  //   state.linkReport = {};
+  //   const {
+  //     reportId,
+  //     linkReportId,
+  //     linkSubItem,
+  //     linkDataType,
+  //     linkXmlParam,
+  //     linkXmlData,
+  //     linkReportOrdinal,
+  //     linkReportType,
+  //     linkReportNm
+  //   } = action.payload;
+  //   const matchSubLinkInfo ={
+  //     reportId: reportId,
+  //     linkReportId: linkReportId,
+  //     subLinkItemId: linkSubItem,
+  //     dataLinkType: linkDataType,
+  //     subLinkXmlParam: linkXmlParam,
+  //     subLinkXmlData: linkXmlData,
+  //     subLinkReportOrdinal: linkReportOrdinal,
+  //     subLinkReportType: linkReportType,
+  //     sublinkReportNm: linkReportNm,
+  //     subLinkParamInfo: []
+  //   };
+  //   const matchLinkInfo = {
+  //     reportId: reportId,
+  //     linkReportId: linkReportId,
+  //     linkXmlParam: linkXmlParam,
+  //     linkReportOrdinal: linkReportOrdinal,
+  //     linkReportType: linkReportType,
+  //     dataLinkType: linkDataType,
+  //     linkReportNm: linkReportNm,
+  //     linkParamInfo: [],
+  //     linkFkInfo: [],
+  //     subYn: 'True',
+  //     subLinkReport: matchSubLinkInfo
+  //   };
+  //   state.linkReport[linkReportId] = matchLinkInfo;
+  // }
 };
 
 const extraReducers = {};
