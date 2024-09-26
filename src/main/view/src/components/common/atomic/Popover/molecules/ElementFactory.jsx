d3de7e9bcd2aa33ffefcfabb10518274d442746d
hoihufu <103414954+hoihufu@users.noreply.github.com>
2024-09-26 09:22:25 +0900

    불필요한 항목 삭제

--- src/main/view/src/components/common/atomic/Popover/molecules/ElementFactory.jsx
+++ src/main/view/src/components/common/atomic/Popover/molecules/ElementFactory.jsx
@@ -8,15 +8,13 @@ import downloadDefaultElement
   from '../../Ribbon/popover/organism/DownloadDefaultElement';
 import datasetDefaultElement
   from '../../Ribbon/popover/organism/DatasetDefaultElement';
-import linkReportListDefaultElement
-  from '../../Ribbon/popover/organism/LinkReportListDefaultElement';
+
 
 const ElementFactory = () => {
   const {saveElement} = saveDefaultElement();
   const customChartElement = customChartDefaulElement();
   const normalChartElement = normalChartDefaultElement();
   const downloadElement = downloadDefaultElement();
-  const linkReportListElement = linkReportListDefaultElement();
   const {getDatasetElement} = datasetDefaultElement();
 
   return {
@@ -24,8 +22,7 @@ const ElementFactory = () => {
     'add_default_chart': normalChartElement,
     'download_report': downloadElement,
     'dataset': getDatasetElement(),
-    'save_report': saveElement,
-    'link_report_list': linkReportListElement
+    'save_report': saveElement
   };
 
   // if (id === 'add_custom_chart') {
