5469636e9131a4c3335ad474ef0f37da60c21a9e
hoihufu <103414954+hoihufu@users.noreply.github.com>
2024-10-10 18:40:58 +0900

    홈앤쇼핑 전용: 토큰 패스 비활성화 및 sessionStorage를 통한 필터정보 전달

--- src/main/view/src/components/report/util/LinkedReportUtility.js
+++ src/main/view/src/components/report/util/LinkedReportUtility.js
@@ -29,20 +29,37 @@ export const getSubLinkDim = (item) => {
 
 export const connectLinkedReport = (param, showReportList) => {
   if (param) {
-    // models.Report.generateLinkToken(param).then((res) => {
-    models.Report.generateToken(param).then((res) => {
-      if (res.status != 200) return;
-      const token = res.data;
-      const urlString =
-        `${getFullUrl()}/linkviewer?token=${token}` +
-        (showReportList ? '&srl=true' : '');
-      const newWindow = window.open(urlString, '_blank');
-      newWindow.focus();
-    }).catch((error) => {
-      console.error('Error sending link report:', error);
-    });
+    const {reportId, reportType, newWindowLinkParamInfo} = param;
+    const params = new URLSearchParams(window.location.search);
+    const fldFilter = params.get('fld') || false;
+    const urlString =
+        `${getFullUrl()}/linkviewer?reportId=${reportId}` +
+        `&reportType=${reportType}` +
+        (showReportList ? '&srl=true' : '') +
+        (fldFilter ? '&fld=' + fldFilter : '');
+    sessionStorage.setItem(
+        'newWindowLinkParamInfo',
+        JSON.stringify(newWindowLinkParamInfo)
+    );
+    const newWindow = window.open(urlString, '_blank');
+    newWindow.focus();
   }
 };
+  // if (param) {
+  //   // models.Report.generateLinkToken(param).then((res) => {
+  //   models.Report.generateToken(param).then((res) => {
+  //     if (res.status != 200) return;
+  //     const token = res.data;
+  //     const urlString =
+  //       `${getFullUrl()}/linkviewer?token=${token}` +
+  //       (showReportList ? '&srl=true' : '');
+  //     const newWindow = window.open(urlString, '_blank');
+  //     newWindow.focus();
+  //   }).catch((error) => {
+  //     console.error('Error sending link report:', error);
+  //   });
+  // }
+
 
 export const openNewTab = (param, closeWindow, showReportList) => {
   if (param) {
