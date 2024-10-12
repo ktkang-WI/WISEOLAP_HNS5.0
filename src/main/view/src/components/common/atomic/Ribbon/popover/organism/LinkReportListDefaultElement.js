226371a24fb94408fd706b599a1f5dcbed73690e
hoihufu <103414954+hoihufu@users.noreply.github.com>
2024-10-12 18:42:43 +0900

    hotfix: 연결보고서 조건을 충족하지않을떄 비표시 분기처리

--- src/main/view/src/components/common/atomic/Ribbon/popover/organism/LinkReportListDefaultElement.js
+++ src/main/view/src/components/common/atomic/Ribbon/popover/organism/LinkReportListDefaultElement.js
@@ -10,6 +10,7 @@ import {
   selectCurrentValues
 } from 'redux/selector/ParameterSelector';
 import {useSelector} from 'react-redux';
+// import useModal from 'hooks/useModal';
 
 const ButtonWrapper = styled.div`
   padding: 6px 16px;
@@ -28,42 +29,43 @@ const LinkReportListDefaultElement = () => {
   const currentParameterValues = useSelector(selectCurrentValues);
 
   if (!linkReportList || Object.keys(linkReportList).length === 0) {
-    alert('연결 보고서가 존재하지 않습니다.');
+    return;
+  } else {
+    return (
+      <ButtonColumn>
+        {Object.values(linkReportList).map((report, index) => (
+          <ButtonWrapper key={report.linkReportId || index}>
+            <CommonButton
+              key={report.linkReportId || index}
+              height={'28px'}
+              type='onlyImageText'
+              onClick={() => {
+                const param = {
+                  reportId: report.linkReportId,
+                  reportType: report.linkReportType,
+                  newWindowLinkParamInfo:
+                    report.linkParamInfo.map((linkParam) => {
+                      const paramKey = linkParam.pkParam;
+                      // linkParam 객체를 얕은 복사하여 새로운 객체를 반환
+                      const newLinkParam = {...linkParam};
+                      if (currentParameterValues.hasOwnProperty(paramKey)) {
+                        // 불변성을 유지하면서 value 값을 새로운 객체에 추가
+                        newLinkParam['value'] =
+                          currentParameterValues[paramKey].value;
+                      }
+                      return newLinkParam;
+                    })
+                };
+                connectLinkedReport(param, true);
+              }}
+            >
+              {report.linkReportNm}
+            </CommonButton>
+          </ButtonWrapper>
+        ))}
+      </ButtonColumn>
+    );
   }
-  return (
-    <ButtonColumn>
-      {Object.values(linkReportList).map((report, index) => (
-        <ButtonWrapper key={report.linkReportId || index}>
-          <CommonButton
-            key={report.linkReportId || index}
-            height={'28px'}
-            type='onlyImageText'
-            onClick={() => {
-              const param = {
-                reportId: report.linkReportId,
-                reportType: report.linkReportType,
-                newWindowLinkParamInfo:
-                  report.linkParamInfo.map((linkParam) => {
-                    const paramKey = linkParam.pkParam;
-                    // linkParam 객체를 얕은 복사하여 새로운 객체를 반환
-                    const newLinkParam = {...linkParam};
-                    if (currentParameterValues.hasOwnProperty(paramKey)) {
-                      // 불변성을 유지하면서 value 값을 새로운 객체에 추가
-                      newLinkParam['value'] =
-                        currentParameterValues[paramKey].value;
-                    }
-                    return newLinkParam;
-                  })
-              };
-              connectLinkedReport(param, true);
-            }}
-          >
-            {report.linkReportNm}
-          </CommonButton>
-        </ButtonWrapper>
-      ))}
-    </ButtonColumn>
-  );
 };
 
 export default LinkReportListDefaultElement;
