9b93e0ed5a60e04d2475f1e5d37a20384237b169
김아진 <99698967+wise-ajkim@users.noreply.github.com>
2024-11-28 09:10:20 +0900

    hotfix: 메인 화면 리렌더링 오류 수정

--- src/main/view/src/portal/components/Header.jsx
+++ src/main/view/src/portal/components/Header.jsx
@@ -8,6 +8,7 @@ import {useEffect, useState} from 'react';
 import models from 'models';
 import DropdownBoxReportSearch
   from 'components/common/atomic/Header/atom/DropdownBoxReportSearch';
+import Timer from './Timer';
 
 const Header = ({
   PORTAL_URL,
@@ -18,18 +19,6 @@ const Header = ({
   admin
 }) => {
   const [portalReportList, setPortalReportList] = useState({});
-
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    // 1초마다 현재 시간을 업데이트
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    // 컴포넌트 언마운트 시 인터벌 정리
-    return () => clearInterval(interval);
-  }, []);
 
   useEffect(() => {
     models.Report.getPortalList().then((data) => {
@@ -105,7 +94,7 @@ const Header = ({
           }
         </ul>
         <div className='portal_right_menu'>
-          <h3>{time.toLocaleTimeString()}</h3>
+          <Timer/>
           <DropdownBoxReportSearch
             showAll={true}
             button={[{
