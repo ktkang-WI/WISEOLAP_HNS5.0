e533b6f6d010e011e38f3edff78c3e67a3c817b0
hoihufu <103414954+hoihufu@users.noreply.github.com>
2024-11-29 17:58:04 +0900

    hotfix: 파이 그래프 여러개인경우, 세로 정렬 및 비율 조정안됨 -> 각 수량에 따라 비율 조정되게끔 수정

--- src/main/view/src/components/report/item/pie/Pie.jsx
+++ src/main/view/src/components/report/item/pie/Pie.jsx
@@ -48,61 +48,108 @@ const Pie = ({setItemExports, id, item}) => {
     return pickedColor;
   };
 
-  const pies = seriesMeasureNames.map((dimension, idx) => {
-    return (
-      <>
-        <PieChart
-          id={id}
-          ref={(ref) => (dxRefs.current[idx] = ref)}
-          key={idx}
-          type={meta.pieChartStyle}
-          dataSource={mart.data.data} // mart
-          width={'calc(100%/'+'1'+')'}
-          height={'100%'}
-          style={{float: 'left'}}
-          title={meta.labelEdit.showTitle ? dimension.caption : ''}
-          animation={{
-            duration: 1000,
-            easing: 'easeOutCubic', // 애니메이션
-            enabled: true
-          }}
-          palette={getColor()}
+  const calculatePieStyles = (totalPies) => {
+    let flex;
+    let maxWidth;
+    let height;
+    if (totalPies === 1) {
+      flex = '1 1 100%';
+      maxWidth = '100%';
+      height = '100%';
+    } else if (totalPies === 2) {
+      flex = '1 1 calc(50% - 10px)';
+      maxWidth = 'calc(50% - 10px)';
+      height = '400px';
+    } else if (totalPies <= 4) {
+      flex = '1 1 calc(50% - 10px)';
+      maxWidth = 'calc(50% - 10px)';
+      height = '300px';
+    } else {
+      flex = '1 1 calc(33.33% - 10px)';
+      maxWidth = 'calc(33.33% - 10px)';
+      height = '250px';
+    }
+    return {flex, maxWidth, height};
+  };
+
+  const renderPieChart = (dimension, idx) => (
+    <>
+      <PieChart
+        id={id}
+        ref={(ref) => (dxRefs.current[idx] = ref)}
+        key={idx}
+        type={meta.pieChartStyle}
+        dataSource={mart.data.data}
+        width={'calc(100%/'+'1'+')'}
+        height={'100%'}
+        style={{float: 'left'}}
+        title={meta.labelEdit.showTitle ? dimension.caption : ''}
+        animation={{
+          duration: 1000,
+          easing: 'easeOutCubic', // 애니메이션
+          enabled: true
+        }}
+        palette={getColor()}
+      >
+        <Legend // 범례
+          visible={meta.legend.useLegend}
+          position={meta.legend.position}
+          horizontalAlignment={meta.legend.horizontalAlignment}
+          verticalAlignment={meta.legend.verticalAlignment}
+          itemTextPosition={meta.legend.itemTextPosition}
+          rowCount={2}
+        />
+        <Tooltip
+          enabled={true}
+          contentRender={(e) => customizeOption(e, meta.tooltip, 'tooltip')}
+        />
+        <Series
+          argumentField='arg'
+          valueField={dimension.summaryName}
         >
-          <Legend // 범례
-            visible={meta.legend.useLegend}
-            position={meta.legend.position}
-            horizontalAlignment={meta.legend.horizontalAlignment}
-            verticalAlignment={meta.legend.verticalAlignment}
-            itemTextPosition={meta.legend.itemTextPosition}
-            rowCount={2}
-          />
-          <Tooltip
-            enabled={true}
-            contentRender={(e) => customizeOption(e, meta.tooltip, 'tooltip')}
-          />
-          <Series
-            argumentField='arg'
-            valueField={dimension.summaryName}
+          <Label visible={true}
+            position={meta.labelPosition}
+            customizeText={(e) => customizeOption(e, meta.labelEdit, 'label')}
           >
-            <Label visible={true}
-              position={meta.labelPosition}
-              customizeText={(e) => customizeOption(e, meta.labelEdit, 'label')}
-            >
-              <Connector
-                visible={true}
-                width={0.5}
-              />
-            </Label>
-          </Series>
-          <Size width={'100%'} />
-        </PieChart>
-      </>
-    );
-  });
+            <Connector
+              visible={true}
+              width={0.5}
+            />
+          </Label>
+        </Series>
+        <Size width={'100%'} />
+      </PieChart>
+    </>
+  );
 
   return (
-    <div style={{overflowY: 'auto', height: '100%'}}>
-      {pies}
+    <div
+      style={{
+        overflowY: 'auto',
+        height: '100%',
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '10px',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start'
+      }}
+    >
+      {seriesMeasureNames.map((dimension, idx) => {
+        const styles = calculatePieStyles(seriesMeasureNames.length);
+        return (
+          <div
+            key={idx}
+            style={{
+              flex: styles.flex,
+              maxWidth: styles.maxWidth,
+              height: styles.height,
+              boxSizing: 'border-box'
+            }}
+          >
+            {renderPieChart(dimension, idx)}
+          </div>
+        );
+      })}
     </div>
   );
 };
