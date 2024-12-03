0a1133d26fbcb76d22e5b10eb8cc9050bb6ae843
진승연 <103473652+syjin2@users.noreply.github.com>
2024-12-03 17:39:19 +0900

    fix: 개인보고서 보고서 component treeView -> treeList 로 수정

--- src/main/view/src/components/useInfo/molcule/MyPageReportList.jsx
+++ src/main/view/src/components/useInfo/molcule/MyPageReportList.jsx
@@ -1,40 +1,21 @@
 import CommonButton from 'components/common/atomic/Common/Button/CommonButton';
-import {TreeView} from 'devextreme-react';
 import localizedString from 'config/localization';
 import removeImg from 'assets/image/icon/button/remove_white.png';
-import styled from 'styled-components';
 import folderImg from 'assets/image/icon/report/folder_load.png';
 import dashImg from 'assets/image/icon/report/dash.png';
 import excelImg from 'assets/image/icon/report/excel_file.png';
 import adhocImg from 'assets/image/icon/report/adhoc.png';
 import {DesignerMode} from 'components/config/configType';
-import React, {useCallback, useRef} from 'react';
+import React from 'react';
 import {getTheme} from 'config/theme';
 import useModal from 'hooks/useModal';
 import {deleteReport} from 'models/report/Report';
 import {userFolderData} from 'routes/loader/LoaderConfig';
-
+import StyledTreeList from
+  'components/config/atoms/reportFolderManagement/StyledTreeList';
+import {Column, SearchPanel, Selection}
+  from 'devextreme-react/tree-list';
 const theme = getTheme();
-
-const StyledTreeView = styled(TreeView)`
-  color: ${theme.color.primaryFont};
-  font: ${theme.font.dataSource};
-  padding: 20px;
-  box-sizing: border-box;
-  letter-spacing: -1px;
-  .dx-treeview-toggle-item-visibility {
-    color: ${theme.color.primaryFont};
-  }
-
-  .dx-treeview-item-content {
-    transform: none !important;
-  }
-
-  .dx-scrollable-wrapper {
-    border: 1px solid #D4D7DC;
-    border-radius: 6px;
-  }
-`;
 
 const smallButton = {
   height: '30px',
@@ -51,23 +32,23 @@ const iconMapper = {
 
 const MyPageReportList = ({data, setData, setTreeViewData, setPrevName}) => {
   const {alert, confirm, success} = useModal();
-  const ref = useRef();
   let reportId = null;
 
   const handleItemClick = (e) => {
-    if (e.itemData.type == 'FOLDER') {
+    const itemData = e.data;
+    if (itemData.type == 'FOLDER') {
       reportId = null;
       setPrevName(null);
       setData({});
     } else {
       // TODO : 추후 back단에서 처리 예정.
-      const newData = e.itemData;
-      if (e.itemData && typeof e.itemData.datasetXml === 'string') {
-        newData.datasetXml = JSON.parse(e.itemData.datasetXml);
+      const newData = itemData;
+      if (itemData && typeof itemData.datasetXml === 'string') {
+        newData.datasetXml = JSON.parse(itemData.datasetXml);
         if (newData.datasetXml.datasets.length > 0) {
           newData.datasets = newData.datasetXml.datasets;
-          if (e.itemData.query) {
-            newData.query = _.cloneDeep(JSON.parse(e.itemData.query));
+          if (itemData.query) {
+            newData.query = _.cloneDeep(JSON.parse(itemData.query));
             for (let i = 0; i < newData.datasets.length; i++) {
               if (newData.query[newData.datasets[i].datasetId]?.length > 0) {
                 newData.datasets[i].selectBoxItems =
@@ -81,11 +62,11 @@ const MyPageReportList = ({data, setData, setTreeViewData, setPrevName}) => {
           }
         }
       }
-      if (e.node?.parent?.text) {
-        newData.fldParentNm = e.node?.parent?.text;
+      if (e.node?.parent?.data?.name) {
+        newData.fldParentNm = e.node?.parent?.data?.name;
       }
 
-      setPrevName(e.itemData.name);
+      setPrevName(itemData.name);
       reportId = newData.id;
       setData({...newData});
     }
@@ -120,37 +101,41 @@ const MyPageReportList = ({data, setData, setTreeViewData, setPrevName}) => {
     });
   };
 
-  const itemRender = useCallback((item) => {
-    return (
-      <div className="dx-item-content dx-treeview-item-content">
-        <img width='16px' src={iconMapper[item.type]} className="dx-icon"/>
-        <span>{item.name}</span>
-      </div>
-    );
-  }, []);
-
   return (
     <>
-      <StyledTreeView
-        ref={ref}
+      <StyledTreeList
         height='90%'
         width='100%'
-        items={data}
-        dataStructure="plain"
-        displayExpr="name"
-        selectionMode='single'
-        parentIdExpr="fldParentId"
+        dataSource={data || []}
         keyExpr="id"
-        noDataText={localizedString.noReports}
-        searchEnabled={true}
-        searchEditorOptions={{
-          placeholder: localizedString.search,
-          width: '300px'
-        }}
-        itemRender={itemRender}
-        focusStateEnabled={true}
-        onItemClick={handleItemClick}
-      />
+        parentIdExpr="fldParentId"
+        showColumnHeaders={false}
+        onRowClick={handleItemClick}
+      >
+        <SearchPanel
+          visible={true}
+          width={250}
+        />
+        <Selection mode="single" />
+        <Column
+          dataField="name"
+          caption={localizedString.folderName}
+          cellRender={({row}) => {
+            return (
+              <>
+                <img
+                  style={{marginTop: '1.5px', float: 'left'}}
+                  src={iconMapper[row.data.type]}
+                  height={'17px'}
+                />
+                <span style={{lineHeight: '17px', marginLeft: '5px'}}>
+                  {row.data.name}
+                </span>
+              </>
+            );
+          }}
+        />
+      </StyledTreeList>
       <div style={{
         display: 'flex',
         height: '10%',
